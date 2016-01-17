3bdd0bdfeedbd2eee6b449ff07d62abcf62814cd
Matthias S. Benkmann <matthias@winterdrache.de>
2016-01-17 20:19:20 +0100

    clarify authorship on fs.go

--- http2/fs.go
+++ http2/fs.go
@@ -1,6 +1,6 @@
 // Copyright 2009 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Taken from the Go sources and modified by Matthias S. Benkmann
 
 // HTTP file system request handler
 
